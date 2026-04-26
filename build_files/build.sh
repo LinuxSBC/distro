5edfabda711289009383bceb97289f1833f53d25
LinuxSBC <anna@simmons.ovh>
2026-04-26 13:32:25 -0500

    Add packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,8 +10,15 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
-
+dnf5 install -y \
+    1password 1password-cli \
+    bat fd-find gh tealdeer zoxide \
+    neovim nodejs-bash-language-server shellcheck \
+    cmake gtk4-devel json-glib-devel libevdev-devel libevdev-utils libgtop2-devel libinput-devel libinput-utils meson python3-tkinter rpmdevtools \
+    firefox \
+    gnome-shell-extension-forge gnome-shell-extension-pop-shell gnome-shell-extension-system-monitor \
+    keyd \
+    freerdp globalprotect-openconnect 
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
